0723558c6be747cf4708cac37c94e6b99540440b
David E. James <david.eugene.james@protonmail.com>
2024-10-21 07:03:56 -0400

    + simple retry

--- src/retry_toolkit/__init__.py
+++ src/retry_toolkit/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: 2024-present David E. James <david.eugene.james@protonmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+from .simple import *
---
+++ src/retry_toolkit/simple.py
@@ -0,0 +1,64 @@
+
+import time
+from functools import wraps
+
+# Backoff time calculation functions
+def exponential(n, b):
+    def _exponential(x):
+        return x**n + b
+    return _exponential
+
+
+def linear(m, b=0):
+    def _linear(x):
+        return m*x + b
+    return _linear
+
+
+def _ensure_callable(var, default):
+    if callable(var):
+        return var
+
+    if var is None:
+        return lambda *args, **kwargs: default
+
+    return lambda *args, **kwargs: var
+
+
+DEFAULT_TRIES   = 3
+DEFAULT_BACKOFF = 0
+DEFAULT_EXC     = Exception
+SLEEP_FUNC      = time.sleep
+
+class GiveUp(Exception):
+    pass
+
+def retry(tries=None, backoff=None, exceptions=None):
+
+    tries_f   = _ensure_callable(tries, DEFAULT_TRIES)
+    backoff_f = _ensure_callable(backoff,  DEFAULT_BACKOFF)
+    exc_f     = _ensure_callable(exceptions, DEFAULT_EXC)
+    sleep_f   = SLEEP_FUNC
+
+    @wraps(func)
+    def _retry_wrapper(func):
+        def _retry(*args, **kwargs):
+            n_tries = tries_f()
+            exc     = exc_f()
+
+            for try_num in range(n_tries):
+
+                if try_num > 0:
+                    sleep_f(backoff_f(try_num))
+
+                try:
+                    return func(*args, **kwargs)
+                except exc as e:
+                    pass
+
+            raise GiveUp()
+
+        return _retry
+    return _retry_wrapper
+
+
