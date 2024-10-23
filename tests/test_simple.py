cee9073fdf1ba3a1ec8880093bf050b5ae9ad0a6
David E. James <david.eugene.james@protonmail.com>
2024-10-22 23:38:47 -0400

    + SPDX in test file

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present David E. James <david.eugene.james@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
+
 import time
 
 #-------------------------------------------------------------------------------
