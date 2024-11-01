823ce746feb596252ca5752780c439c727cfcf80
David E. James <david.eugene.james@protonmail.com>
2024-11-01 09:30:31 -0400

    + Retry class

--- src/retry_toolkit/core.py
+++ src/retry_toolkit/core.py
@@ -1,25 +1,51 @@
+#┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅#
+#┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅#
 import functools
 
 
 
+#──────────────────────────────────────────────────────────────────────────────#
+#──────────────────────────────────────────────────────────────────────────────#
 class RetryContextDecorator:
-    def __init__(self, tries, backoff, exceptions):
-        pass
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        self.registry = dict()
 
 
-    def __enter__(self, ...):
+    #┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈#
+    #┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄#
+    def _retry_class(self):
+        return Retry(self)
+
+
+    #┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄#
+    def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
 
-    def __call__(self, func):   #*args, **kwargs):
+    #┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄#
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+
+
+    #┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄#
+    def __call__(self, func):
+        _retry = _retry_class(self)
+        self.registry[func.__name__] = _retry
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             with self:
-                self._execute_with_retry(func, *args, **kwargs)
+                return _retry.exec(*args, **kwargs)
+
         return wrapper
 
+
+
+class Retry:
+    def __init__(self, parent):
+        pass
 
     def _execute_with_retry(self, func, *args, **kwargs):
         self._setup()
@@ -31,50 +57,38 @@ class RetryContextDecorator:
             except excs_to_catch as e:
                 self._exception(e)
 
-        self._giveup()
+        self._giveup(try_num, func)
 
 
     def _setup(self):
         # configure at call-time to allow any changes to defaults
         # to properly take effect each time func is used
-        n_tries_f = _ensure_callable(tries      , Defaults.TRIES  )
-        backoff_f = _ensure_callable(backoff    , Defaults.BACKOFF)
-        exc_f     = _ensure_callable(exceptions , Defaults.EXC    )
-        sleep_f   = Defaults.SLEEP_FUNC
+        self.n_tries_f = _ensure_callable(tries      , Defaults.TRIES  )
+        self.backoff_f = _ensure_callable(backoff    , Defaults.BACKOFF)
+        self.exc_f     = _ensure_callable(exceptions , Defaults.EXC    )
+        self.sleep_f   = Defaults.SLEEP_FUNC
 
-        n_tries = n_tries_f()
-        exc     = exc_f()
+        self.n_tries = n_tries_f()
+        self.exc     = exc_f()
 
         # context/state
-        total_sleep    = 0.0
-        exception_list = []
+        self.total_sleep    = 0.0
+        self.exception_list = []
 
 
     def _sleep(self, try_num):
         if try_num > 0:
-            sleep_time = backoff_f(try_num-1)
+            sleep_time = self.backoff_f(try_num-1)
             total_sleep += sleep_time
-            sleep_f(sleep_time)
+            self.sleep_f(sleep_time)
 
 
     def _exception(self, e):
-        exception_list.append(e)
+        self.exception_list.append(e)
 
 
-    def _giveup(self):
-        raise GiveUp(try_num+1, total_sleep, func, exception_list)
-
-
-
+    def _giveup(self, try_num):
+        raise GiveUp(try_num+1, self.total_sleep, self.func, self.exception_list)
 
 
 
-    def _foo(self):
-        for try_num in range(n_tries):
-
-
-            try:
-                return func(*args, **kwargs)
-            except exc as e:
-
-        raise GiveUp(try_num+1, total_sleep, func, exception_list)
